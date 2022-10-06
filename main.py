50a09603a863f66ba3e75186b92c6240bb93cd92
rahulranjan74 <106507426+rahulranjan74@users.noreply.github.com>
2022-10-06 22:44:32 +0530

    Added Space invader game
    using pygame
    added important images

---
+++ main.py
@@ -0,0 +1,89 @@
+import random
+
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+icon = pygame.image.load('spaceship.png')
+pygame.display.set_caption("Space Invaders")
+background = pygame.image.load('5512626.jpg')
+
+playerImg = pygame.image.load('spaceship.png')
+playerX = 370
+playerY = 480
+playerX_change = 0.3
+playerY_change = 0
+
+enemyImg = pygame.image.load('ufo.png')
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyX_change = 0.1
+enemyY_change = 40
+
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 10
+bullet_state = "ready"
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
+
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
+
+
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    screen.blit(background, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.1
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.1
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX_change = 0.2
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change = -0.2
+        enemyY += enemyY_change
+    if bulletY <= 0:
+        bulletY = 480
+        bulletY = "ready"
+    if bullet_state == "fire":
+        fire_bullet(playerX, bulletY)
+        bulletY -= bulletY_change
+
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
+    pygame.display.update()
